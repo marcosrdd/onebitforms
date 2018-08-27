6ea53117bc8271710d1b8355583384c112c2a0d6
Marcos Robson Dias Duarte <marcosrdd@gmail.com>
2018-08-27 13:56:09 -0300

    install devise token auth

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+config.include Devise::Test::ControllerHelpers, type: :controller
 
 # Add additional requires below this line. Rails is not loaded until this point!
 
