a6283fcdb877c180afd9c556e10836bd83b7e012
Marcos Robson Dias Duarte <marcosrdd@gmail.com>
2018-09-04 10:26:29 -0300

    Fixed form and question test controller

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,11 +1,11 @@
 class Answer < ApplicationRecord
   belongs_to :form
   has_many :questions_answers, dependent: :destroy
-  accepts_nested_attributes_for :questions_answers
+  # accepts_nested_attributes_for :questions_answers
   validates :form, presence: true
-  
- 
-  def self.create_with_questions form, questions_answers
+
+
+  def self.create_with_questions_answers form, questions_answers
     answer = nil
     ActiveRecord::Base.transaction do
       answer = Answer.create(form: form)
