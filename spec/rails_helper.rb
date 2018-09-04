02001173c452809040a0acf89d2623a5b8f73f43
Marcos Robson Dias Duarte <marcosrdd@gmail.com>
2018-09-04 08:59:18 -0300

    Implemented the form controller file

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,11 +7,6 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
-config.include Devise::Test::ControllerHelpers, type: :controller
-# Custom json helpers
-config.include Requests::JsonHelpers, type: :request
-# Custom Header helpers
-config.include Requests::HeaderHelpers, type: :request
 
 # Add additional requires below this line. Rails is not loaded until this point!
 
@@ -46,6 +41,13 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  # Custom json helpers
+  config.include Requests::JsonHelpers, type: :request
+  # Custom Header helpers
+  config.include Requests::HeaderHelpers, type: :request
+
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
